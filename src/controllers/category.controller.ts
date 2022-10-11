aa83a3db10058c593e8e6727ab3028e189c302cf
hosybinhkog <hosybinhkog@gmail.com>
2022-10-12 00:20:12 +0700

    update category

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -5,11 +5,52 @@ import { Category } from "../models";
 
 export const getCategories = catchAsyncError(async (_req, res, _next) => {
   const categories = await Category.find({});
-
+  const categoriesCount = await Category.count();
   res.status(200).json({
     categories,
     message: "get categories successfully",
     success: true,
+    categoriesCount,
+  });
+});
+
+export const getCategoryById = catchAsyncError(async (req, res, next) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) return next(new ErrorHandler("Category not found", 404));
+  return res.status(200).json({
+    success: true,
+    message: "get category sucessfully!!!",
+    category,
+  });
+});
+
+export const updateCategory = catchAsyncError(async (req, res, next) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) return next(new ErrorHandler("Category not found", 404));
+  if (req.body.img !== undefined) {
+    await cloundinary.v2.uploader.destroy(category!.img!.public_id);
+
+    const result = await cloundinary.v2.uploader.upload(req.body.img, {
+      folder: "categories",
+    });
+
+    req.body.img = { public_id: result.public_id, url: result.secure_url };
+  }
+
+  const updateCategory = await Category.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  await updateCategory?.save();
+
+  res.status(200).json({
+    success: true,
+    updateCategory,
+    message: "update category success",
   });
 });
 
--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -12,4 +12,13 @@ categoryRoute.post(
   categoryController.createCategory
 );
 
+categoryRoute.get("/:id", categoryController.getCategoryById);
+
+categoryRoute.put(
+  "/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  categoryController.updateCategory
+);
+
 export default categoryRoute;
